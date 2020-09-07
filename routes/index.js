9bfb7e8df9ceef9f1350ee403ed9341267b5c804
neul <neul@smithground.com>
2020-09-07 23:51:13 +0900

    HN- /todo uri

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/todo', function(req, res, next) {
+  res.render('todo', { title: 'Express' });
+});
+
 module.exports = router;
